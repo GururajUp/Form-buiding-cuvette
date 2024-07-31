6c3b83b4e9ddbb50738d2c9e15bcaadbb6807d29
GururajUp <gururajupadhyaya11@gmail.com>
2024-08-01 00:27:37 +0530

    display

--- server/index.js
+++ server/index.js
@@ -13,12 +13,12 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const corsOptions = {
-    origin: 'http://localhost:5173', 
-    optionsSuccessStatus: 200
-};
+// const corsOptions = {
+//     origin: 'http://localhost:5173', 
+//     optionsSuccessStatus: 200
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 
 // Connect to MongoDB
