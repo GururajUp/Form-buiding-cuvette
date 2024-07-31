ec0782b3f11cbc9a03258c621d6606960b53a1d4
GururajUp <gururajupadhyaya11@gmail.com>
2024-08-01 00:04:22 +0530

    display

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(express.static(path.join(_dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 const corsOptions = {
     origin: 'http://localhost:5173', 
