ffeead1cd5c8cd4c3ded6bcaa6de899641016b03
Gururaj Upadhyaya <160821178+GururajUp@users.noreply.github.com>
2024-08-01 14:13:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,12 +13,7 @@ app.use(express.json());
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
-const corsOptions = {
-    origin: 'http://localhost:5173', 
-    optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 // Connect to MongoDB
